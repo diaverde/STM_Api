2824abd97075166d305ae48847464bd14fae2970
Andrés Sánchez <diaverde77@gmail.com>
2025-06-21 10:30:43 -0500

    New attempt - Fixed routes in python script

--- stm-python/stmDataToSqlite.py
+++ stm-python/stmDataToSqlite.py
@@ -1,13 +1,14 @@
 import csv, sqlite3, os
 
 STM_DIR = 'gtfs_stm'
-con = sqlite3.connect("../stmDB.sqlite") # change to 'sqlite:///your_filename.db'
 
+con = sqlite3.connect("../stmDB.sqlite")
 cur = con.cursor()
 
 # Routes
 cur.execute("CREATE TABLE routes (route_id integer PRIMARY KEY,agency_id,route_short_name,route_long_name,route_type,route_url,route_color,route_text_color);") # use your column names here
-file_path = os.path.join(STM_DIR, 'routes.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'routes.txt')
+#file_path = os.path.abspath(os.path.join(os.sep, STM_DIR, 'routes.txt'))
 with open(file_path,'r') as fin:
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter    
@@ -23,7 +24,7 @@ cur.executemany("INSERT INTO routes (route_id,agency_id,route_short_name,route_l
 
 # Stops
 cur.execute("CREATE TABLE stops (stop_id PRIMARY KEY,stop_code,stop_name,stop_lat,stop_lon,stop_url,location_type,parent_station,wheelchair_boarding);")
-file_path = os.path.join(STM_DIR, 'stops.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'stops.txt')
 with open(file_path,'r') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter    
     #for i in dr:
@@ -34,7 +35,7 @@ cur.executemany("INSERT INTO stops (stop_id,stop_code,stop_name,stop_lat,stop_lo
 
 # Shapes
 cur.execute("CREATE TABLE shapes (shape_id integer,shape_pt_lat,shape_pt_lon,shape_pt_sequence integer);")
-file_path = os.path.join(STM_DIR, 'shapes.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'shapes.txt')
 with open(file_path,'r') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter    
     #for i in dr:
@@ -45,7 +46,7 @@ cur.executemany("INSERT INTO shapes (shape_id,shape_pt_lat,shape_pt_lon,shape_pt
 
 # Services
 cur.execute("CREATE TABLE services (service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date);")
-file_path = os.path.join(STM_DIR, 'calendar.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'calendar.txt')
 with open(file_path,'r') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter    
     #for i in dr:
@@ -56,7 +57,7 @@ cur.executemany("INSERT INTO services (service_id,monday,tuesday,wednesday,thurs
 
 # Trips
 cur.execute("CREATE TABLE trips (route_id integer,service_id,trip_id integer PRIMARY KEY,trip_headsign,direction_id,shape_id integer,wheelchair_accessible,note_fr,note_en);")
-file_path = os.path.join(STM_DIR, 'trips.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'trips.txt')
 with open(file_path,'r') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter    
     #for i in dr:
@@ -67,7 +68,7 @@ cur.executemany("INSERT INTO trips (route_id,service_id,trip_id,trip_headsign,di
 
 # Stop times
 cur.execute("CREATE TABLE stop_times (trip_id integer,arrival_time,departure_time,stop_id,stop_sequence integer);")
-file_path = os.path.join(STM_DIR, 'stop_times1.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'stop_times1.txt')
 with open(file_path,'r') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter    
     #for i in dr:
@@ -76,14 +77,14 @@ with open(file_path,'r') as fin:
     
 cur.executemany("INSERT INTO stop_times (trip_id,arrival_time,departure_time,stop_id,stop_sequence) VALUES (?, ?, ?, ?, ?);", stop_times_db)
 
-file_path = os.path.join(STM_DIR, 'stop_times2.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'stop_times2.txt')
 with open(file_path,'r') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter    
     stop_times_db = [(int(i['trip_id']),i['arrival_time'],i['departure_time'],i['stop_id'],int(i['stop_sequence'])) for i in dr]
     
 cur.executemany("INSERT INTO stop_times (trip_id,arrival_time,departure_time,stop_id,stop_sequence) VALUES (?, ?, ?, ?, ?);", stop_times_db)
 
-file_path = os.path.join(STM_DIR, 'stop_times3.txt')
+file_path = os.path.join(os.getcwd(), STM_DIR, 'stop_times3.txt')
 with open(file_path,'r') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter    
     stop_times_db = [(int(i['trip_id']),i['arrival_time'],i['departure_time'],i['stop_id'],int(i['stop_sequence'])) for i in dr]
